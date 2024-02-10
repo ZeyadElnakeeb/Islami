4a73cebf45826580916597f128fc7f76cd4c6ea9
ZeyadElnakeeb <zyadelnakeeb87@gmail.com>
2024-02-10 12:23:58 -0800

    i finished bottom Navigation Bar and make 4 fragments

--- app/src/main/java/com/example/islamy/HomeActivity.kt
+++ app/src/main/java/com/example/islamy/HomeActivity.kt
@@ -2,10 +2,51 @@ package com.example.islamy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.islamy.databinding.ActivityMainBinding
+import com.example.islamy.fragments.HadethFragment
+import com.example.islamy.fragments.QuranFragment
+import com.example.islamy.fragments.RadioFragment
+import com.example.islamy.fragments.SebhaFragment
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        replaceFragment(QuranFragment())
+
+binding.bottomNavView.setOnItemSelectedListener {view->
+
+    when(view.itemId){
+
+        R.id.quran->replaceFragment(QuranFragment())
+        R.id.hadeth->replaceFragment(HadethFragment())
+        R.id.sebha->replaceFragment(SebhaFragment())
+        R.id.radio->replaceFragment(RadioFragment())
+
+        else->{
+
+        }
+
     }
+    true
+
+
+
+}
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view,fragment)
+            .commit()
+
+
+    }
+
 }
